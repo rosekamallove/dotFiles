36567709865e93168dfd0c7567cff03742fba88e
Rose Kamal Love <xyz@gmail.com>
2021-02-20 22:36:07 +0530

    update to vim config

--- .vim/templates/code.cpp
+++ .vim/templates/code.cpp
@@ -1,73 +1,37 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
 
-/*=> Macros <===================================================================>*/
+#define F first
+#define S second
+#define pb push_back
+#define mp make_pair
+#define int ll
+#define fn(i,a,b) for (int i=a; i<=a; i++)
 
-#define 	fo(i,n) for(i=0; i<n; i++)
-#define 	ll long long
-#define 	pb push_back
-#define 	mp make_pair
-#define 	all(x) s.begin(), x.end()
-#define 	tr(it, a) for(auto it = a.begin(): it != a.end(): it++) //toIterateOverSet
-#define 	sortall(x) sort(all(x))
-#define 	deb(x) cout << #x << "=" << x << endl //#x-> variable name
-#define 	deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
-#define 	PI 3.1415926535897932384626
-
-/*<=================================================================================>*/
-typedef 	pair<int, int> pii;
-typedef 	pair<ll, ll> pl;
-typedef 	vector<int> vi;
-typedef 	vector<ll> vl;
-typedef 	vector<pii> vpii; //vector of pairs
-typedef 	vector<pl> vpl;
-typedef 	vector<vi> vvi; //vector of vector
-typedef 	vector<vl> vvl;
-
-/*<==> FuntionsAndGlobalStuff <=========================================================>*/
-
-void inputOutputFile(); //Takes input from input.txt and prints output to output.txt
-const int mod = 1'000'000'007;
-const int N = 3e5, M=N;
-
-/*<====================================================================================>*/
-
-vector<int> r;
-int a[N];
+void fileIO();
 
 void solve()
 {
-    int i, j, n, m;
 }
 
 
-int main()
+int32_t main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);//for faster IO
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-  inputOutputFile();
-  int t=1; cin>>t;
-  while(t--)
-  {
-      solve();
-  }
+  int t; cin>>t;
+  while(t--) solve();
+
   return 0;
 }
 
-
-
-/*<==> FunctionImplementations <=======================================================*/
-
-void inputOutputFile()
+void fileIO()
 {
-#ifndef ONLINE_JUDGE
-    //for getting input from input.txt
-    freopen("input.txt", "r", stdin);
-    //for writing output to output.txt
-    freopen("output.txt", "w", stdout);
-#endif
+  freopen("input.txt", "r", stdin);
+  cin.tie(0);
 }
-
-
--- .vimrc
+++ .vimrc
@@ -40,6 +40,7 @@ noremap sv :w<CR>
 "CPP Snippets:
 """""""""""""""""""""""""""""""""""""""
 noremap bcpp :r ~/.vim/templates/basic.cpp<CR>
+noremap cpp :r ~/.vim/templates/code.cpp<CR>
 noremap runc :!g++  %  ; ./a.out<CR>
 
 """""""""""""""""""""""""""""""""""""""
@@ -82,7 +83,6 @@ call plug#begin()
         Plug 'severin-lemaignan/vim-minimap'
         Plug 'airblade/vim-gitgutter'
         Plug 'vim-airline/vim-airline-themes'
-	Plug 'neoclide/coc.nvim', {'branch':'release'}
   
 "    Plug 'itchyny/lightline.vim'
 call plug#end()
