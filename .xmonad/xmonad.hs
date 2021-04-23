7c3e1c07569c580a058ed9dd4eb2ad641751b021
rosekamallove <rosekamallove@gmail.com>
2021-04-23 12:49:49 +0530

    xmonad and xmobar changed the bg color and fonts on xmobar updated xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -61,8 +61,8 @@ myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 
 -- Border colors for unfocused and focused windows, respectively.
 --
-myNormalBorderColor  = "#9e3f3f"
-myFocusedBorderColor = "#ff5757"
+myNormalBorderColor  = "#282c34"
+myFocusedBorderColor = "#9e3f3f"
 
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
