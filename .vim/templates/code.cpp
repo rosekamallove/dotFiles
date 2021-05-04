a7c2b92ab5bfe5ad9bc53c8771e8e8823d01bfb6
rosekamallove <rosekamallove@gmail.com>
2021-05-04 23:22:37 +0530

    new CPP Template

--- .vim/templates/code.cpp
+++ .vim/templates/code.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define fastIO() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define int long long
 #define endl "\n"
-#define deb(x)        cout << #x << ": " << x << endl
-#define tdeb(x,y)     cout << #x << ": " << x << #y << ": " << y << endl
+#define deb(x)        cout << #x << " ++> "; print(x)
+#define tdeb(x,y)     cout <<'('<< #x<<','<<#y<<") ++> "; print(x,y)
 #define rep(i,n)      for (int i(0); i<n ;  i++)
 #define rip(i,n)      for (int i(1); i<=n;  i++)
 #define rap(i,a,n)    for (int i(a); i<n ;  i++)
-void fastIO();
+
+template<typename T> void print(vector<T> v);
+template<typename T> void print(T n);
+template<typename T, typename Tt> void print(T n, Tt m);
+
 
 void solve()
 {
@@ -23,8 +28,19 @@ signed main()
         solve();
 }
 
-void fastIO()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+template<typename T, typename Tt> void print(T n, Tt m){
+    cout<<'{'<<n<<','<<m<<'}'<<endl;
+}
+
+template<typename T> void print(T n){
+    cout<<n<<endl;
+}
+
+template<typename T> void print(vector<T> v) {
+    cout<<'[';
+    for(auto val : v){
+        cout<<val;
+        cout<<' ';
+    }
+    cout<<']'<<endl;
 }
